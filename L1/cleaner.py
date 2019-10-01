870ecd9aeb83dceb6c5972c34b4e9f99aeecfc32
feliperamis <feliperamisv@gmail.com>
2019-10-02 00:27:15 +0200

    Añadido heaps law, y los output

--- L1/cleaner.py
+++ L1/cleaner.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import json
+import json, os
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
@@ -16,7 +16,7 @@ def es_palabra_valida(palabra):
           return False
       return True
 
-def palabras_validas(archivo):
+def coger_palabras_validas(archivo):
   frecuencias = []
   rangos = []
 
@@ -29,44 +29,65 @@ def palabras_validas(archivo):
             frecuencias.append(int(palabra[0].split(',')[0]))
             rangos.append(i)
             i += 1
-
+  frecuencias.reverse()
   return frecuencias,rangos
 
-def func(x,a,b,c):
+def zipsLaw(x,a,b,c):
+    """ Función de la ley de zipf """
     a = 1.05
     return c/((x+b)**a)
 
-def make_fitPlot(rangos,frecuencias):
-    popt, pcov = curve_fit(func, rangos, frecuencias)
-    print(popt)
-    plt.plot(rangos,func(rangos,*popt),'r--',linewidth=0.8,label='fit')
+def heapsLaw(numeroPalabras, k , beta):
+  return k * (numeroPalabras**beta)
 
-def make_plot(ARCHIVO):
-    frecuencias,rangos = palabras_validas(ARCHIVO)
-    frecuencias.reverse()
+def make_fitPlot(rangos, frecuencias, funcion_fit):
+    popt, pcov = curve_fit(funcion_fit, rangos, frecuencias)
+    plt.plot(rangos,funcion_fit(rangos,*popt),'g--', linewidth = 0.8, label='fit')
+    return popt
 
+def make_plot(frecuencias, rangos, funcion_fit = zipsLaw, archivo_a_guardar = ''):
     maxI = 10000
 
 # Gráfico
-
     # Datos Reales
-    plt.plot(rangos[0:maxI], frecuencias[0:maxI],'b',label='data')
+    plt.plot(rangos[0:maxI], frecuencias[0:maxI], 'b', label='data')
 
     # Fitting curve
-    make_fitPlot(rangos[0:maxI],frecuencias[0:maxI])
+    valuesFound = make_fitPlot(rangos[0:maxI], frecuencias[0:maxI], funcion_fit)
 
     # Configuramos el plot
-    plt.grid(b=True,color="black",linestyle="--")
+    plt.grid(b = True,color = "black",linestyle = "--")
     plt.legend()
     plt.xlabel("x = Rango de las palabras")
     plt.ylabel("y = Frecuencia de la palabra")
 
-
-    plt.savefig(ARCHIVO[0:len(ARCHIVO)-3]+"png")
-    plt.close()
+    if archivo_a_guardar != '':
+      plt.savefig(archivo_a_guardar+".png")
+      # Se guardan los valores de las funciones.
+      # c,b,alpha para zips. k y beta para heaps
+      with open(archivo_a_guardar + 'Values.txt', 'w+') as archivo:
+        archivo.write(np.array2string(valuesFound))
+      plt.close()
+    else:
+      plt.show()
 
 if __name__ == '__main__':
-  make_plot("novels.txt")
+  frecuencias, rangos = coger_palabras_validas('novels.txt')
+  """make_plot(
+    frecuencias = frecuencias,
+    rangos = rangos,
+  )"""
+  path_novel_heaps = 'output/novel_heaps'
+  for index in os.listdir(path_novel_heaps):
+    frecuencias, rangos = coger_palabras_validas('novel_heaps/' + index)
+    make_plot(
+      frecuencias = frecuencias,
+      rangos = rangos,
+      funcion_fit = heapsLaw,
+      archivo_a_guardar = 'images/' + index.split('.')[0]
+    )
+
+  
  # Aquest conjunt de dades no troba un optim per una a fixada = 1
  # make_plot("arxivs.txt")
  # make_plot("news.txt")
