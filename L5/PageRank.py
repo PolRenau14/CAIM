f6e9655464488e8f68c87284d98e4a979b13fc78
PolRenau14 <pol.renau@est.fib.upc.edu>
2019-11-15 21:01:58 +0100

    PageRank acabat dona prop de 1

--- L5/PageRank.py
+++ L5/PageRank.py
@@ -7,13 +7,16 @@ PageRank
 :Description: PageRank
     Compute a simulation page rank but with airports example.
     It's missing when one node is not connected ( no ones travel to him)
+    Compute weight of repeated edges into one same, but getting sum of weights.
+
+    Podem borrar desti de edges ja que no el fem servir.
 
 
 :Authors:
     Pol
 
 :Version:
-    1.0
+    1.1
 
 :Date:
     07/11/2019
@@ -27,7 +30,6 @@ import sys
 class Edge:
     def __init__ (self, origin=None,desti=None,index=None):
         self.origin = origin # write appropriate value
-        self.desti = desti
         self.weight = 1.0 # write appropriate value
         self.index = index
 
@@ -46,20 +48,27 @@ class Airport:
         self.pageIndex = pageIndex      # this value indicates the position of edgeList.
 
     def __repr__(self):
-        return "{0}\t{2}\t{1}\t{3}".format(self.code, self.name, self.pageIndex,self.routes)
+        return "{0}\t{2}\t{1}\t{3}".format(self.code, self.outweight, self.pageIndex,self.routes)
 
-    def addRoute(self,e=Edge()):
-        self.routes.append(e)
-        self.routeHash[e.origin] = e
+    def addRoute(self,origin):
+        #si la edge ja esta, hem d'incrementar el pes d'aquesta.
+        if origin in self.routeHash: #esta en la lista.
+            self.routes[self.routeHash[origin]].weight += 1.0
+        else:
+            e = Edge()
+            e.origin = origin
+            self.routes.append(e)
+            self.routeHash[origin] = len(self.routes)-1 #guardem el index de la pos de routes
 
-edgeList = [] # list of Edge
-edgeHash = dict() # hash of edge to ease the match: clau Index.
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
 
 def getAirport(code):
     if code in airportHash:
-        return airportHash[code]
+        return airportList[airportHash[code]]
+    else:
+        raise Exception("El airoport no esta en la llista de airports")
+        pass
 
 def readAirports(fd):
     print("Reading Airport file from {0}".format(fd))
@@ -79,7 +88,7 @@ def readAirports(fd):
         else:
             cont += 1
             airportList.append(a)
-            airportHash[a.code] = a
+            airportHash[a.code] = a.pageIndex
     airportsTxt.close()
     print("There were {0} Airports with IATA code".format(cont))
 
@@ -87,63 +96,56 @@ def readAirports(fd):
 def readRoutes(fd):
     print("Reading Routes file from {0}".format(fd))
     routesTxt = open(fd,"r")
-    cont = 0
     for line in routesTxt.readlines():
-        e = Edge()
         try:
             temp = line.split(',')
             if len(temp[2]) != 3 or len(temp[4]) != 3:
                 raise Exception('not an IATA code on route')
-            e.origin    = temp[2]
-            e.desti     = temp[4]
-            e.index     = cont
-            airportOrigen = getAirport(e.origin)
-            airportDesti = getAirport(e.desti)
+            airportOrigen = getAirport(temp[2])
+            airportDesti = getAirport(temp[4])
 
             airportOrigen.outweight += 1.0
-            airportDesti.addRoute(e)
+            airportDesti.addRoute(temp[2])
 
         except Exception as inst:
             pass
-        else:
-            cont += 1
-            edgeList.append(e)
-            edgeHash[e.origin] = e
     routesTxt.close()
-    print("There where {0} edges with IATA code".format(cont))
+    print("There where {0} edges with IATA code".format(0))
     # write your code
 
-def makeDiff(v1,v2):
-    dif = 0
-    for i in range(0,len(v1)):
-        dif += v1[i]-v2[i]
-    return dif
-
+def getNumOuts():
+    cont = 0
+    for a in airportList:
+        if len(a.routes) == 0 and a.outweight == 0:
+            cont += 1
+    return cont
 
 
 def computePageRanks():
     # write your code
     print("ComputingPageRanks")
     n = len(airportList)
-    P = [1/n for i in range(n)]
-    L = 0.5                     # quan més gran és més iteracions realitza.
-    thresHold = 0.00000000006
+    P = [1.0/n for i in range(n)]
+    L = 0.85                    # quan més gran és més iteracions realitza.
+    thresHold = 0.00000006
     dif = 1.0
     iterations = 0
-
+    aux2 = 1.0/float(n)
+    numberOuts = L/float(n)*getNumOuts()
     def getSum(i):
         sum = 0
         for eVal in airportList[i].routes:
-            j = airportHash[eVal.origin].pageIndex
+            j = airportHash[eVal.origin]
             aux = P[j] * eVal.weight / airportList[j].outweight
             sum += aux
         return sum
 
     while dif > thresHold :
-        Q = [0 for i in range(n)]
-        for i in range(0,n-1):
-            Q[i] = L * getSum(i) + (1-L)/n
-        dif = sum( [ p-q for (p,q) in zip(P,Q)])
+        Q = [0 for k in range(n)]
+        for i in range(n):
+            Q[i] = L * getSum(i) + float((1-L)/n) + aux2*numberOuts
+        aux2 = (1-L)/n + aux2*numberOuts
+        dif = sum( [ abs(p-q) for (p,q) in zip(P,Q)])
         P = Q
         iterations += 1
     return iterations,P
@@ -161,7 +163,8 @@ def main(argv=None):
     time1 = time.time()
     iterations,pageRanks = computePageRanks()
     time2 = time.time()
-    outputPageRanks(pageRanks)
+    print(sum(pageRanks))
+    #outputPageRanks(pageRanks)
     print("#Iterations:", iterations)
     print("Time of computePageRanks():", time2-time1)
 
